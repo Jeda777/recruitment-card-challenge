8632d11a396e56c0c931ca0c8b83810e0b9ccbdf
M-art-a <marta.2706@wp.pl>
2025-03-26 15:11:52 +0100

    Initial commit

--- js/modules/custom.js
+++ js/modules/custom.js
@@ -1,2 +1,17 @@
-export default function(){
+export default function () {
+  const cardNumberInput = document.querySelector("#card__form--input-number");
+  const cardPreviewNumber = document.querySelector(".card__preview--number");
+  const cardHolderInput = document.querySelector("#card__form--input-holder");
+  card;
+  const expirationDateMonthInput = document.querySelector(
+    "#card__form--select-month"
+  );
+  const expirationDateYearInput = document.querySelector(
+    "#card__form--select-year"
+  );
+  const cardCWInput = document.querySelector("#card__form--input-cw");
+
+  cardNumberInput.addEventListener("input", (e) => {
+    console.log("fdsfs");
+  });
 }
